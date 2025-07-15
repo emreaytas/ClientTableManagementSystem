32141b94482913209337011188c09490dd47967b
EMRE AYTAS <emreayta01@gmail.com>
2025-07-16 00:02:21 +0300

    anabileşenler tamamlandı.

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav></nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
